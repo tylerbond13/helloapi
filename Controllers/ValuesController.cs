fafb0f22c429f1368b8eb21c5e301e5a154c8c0c
tylerbond13 <tyler.robert.bond@gmail.com>
2017-01-30 10:55:21 -0500

    Changed value to 5

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace helloapi.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value 5";
         }
 
         // POST api/values
